3397e1e76f6e405d135135af4aa3be4fe8532df5
ggboy-shakalaka <switch2018@outlook.com>
2021-12-19 12:35:15 +0800

    spring cloud eureka 优化

--- demo/demo-eurekaClient/src/main/java/com/zhaizq/framework/demo/eurekaClient/controller/BaseController.java
+++ demo/demo-eurekaClient/src/main/java/com/zhaizq/framework/demo/eurekaClient/controller/BaseController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
 public class BaseController {
     @Value("${server.port}")
     private Integer port;
---
+++ demo/demo-eurekaServer/src/main/java/com/zhaizq/framework/demo/eurekaServer/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.zhaizq.framework.demo.eurekaServer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.server.EurekaController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class BaseController {
+    @Autowired
+    private EurekaController eurekaController;
+
+    @RequestMapping("/switch")
+    public String action(HttpServletRequest request, Map<String, Object> model) {
+        eurekaController.status(request, model);
+        return "eureka/switch";
+    }
+}
